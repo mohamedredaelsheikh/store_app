72ad8c7e71841e2e97db0c206fe52f7cd6c987d1
Mohammed_Elsheikh <moh.reda198@gmail.com>
2023-11-28 03:27:21 +0200

    Create Put Method

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -40,4 +40,35 @@ class Api {
       );
     }
   }
+
+  Future<dynamic> put({
+    required String url,
+    @required dynamic body,
+    @required String? token,
+  }) async {
+    Map<String, String> headers = {};
+
+    headers.addAll(
+      {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    );
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+
+    http.Response response = await http.put(
+      Uri.parse(url),
+      body: body,
+      headers: headers,
+    );
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+      return data;
+    } else {
+      throw Exception(
+        "There is a problem with status code${response.statusCode}with body${jsonDecode(response.body)}",
+      );
+    }
+  }
 }
